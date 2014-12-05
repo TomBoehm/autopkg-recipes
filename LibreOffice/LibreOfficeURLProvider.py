a9bbf28b133bc4383e252a5f60c34402c345067e
TomBoehm <munkiadmin@munki.lan>
2014-12-05 07:18:12 +0100

    4

--- LibreOffice/LibreOfficeURLProvider.py
+++ LibreOffice/LibreOfficeURLProvider.py
@@ -131,8 +131,8 @@ class LibreOfficeURLProvider(Processor):
         type_component = '='.join(['type', type])
         lang_component = '='.join(['lang', language_code])
         request_string = '&'.join([type_component, lang_component])
-        self.output("TOM - Base: %s" % base_url_start)
-        self.output("TOM - Requ: %s" % request_string)
+        self.output("XXX - Base: %s" % base_url_start)
+        self.output("XXX - Requ: %s" % request_string)
         return '?'.join([base_url_start, request_string])
     
     
